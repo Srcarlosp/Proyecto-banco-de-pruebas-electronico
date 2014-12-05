cddfed2492efb6d0a5f042df11d13529280a4640
Luis Herce Porras <luishercep@gmail.com>
2014-12-05 14:42:19 +0100

    Update Fuente.ino
    Actulizacion de fuentes

--- Fuente.ino
+++ Fuente.ino
@@ -9,6 +9,17 @@ int SerialCK=3;
 int ParallelCK=2;
 int SerialD=1;
 int shiftWrite[8]={1,0,1,0,1,0,1,0};
+//Botones
+int estadoActualBotones[8];
+int estadoAnteriorBotones[8]={0,0,0,0,0,0,0,0};
+//Fuentes y voltimetros
+int voltimetro0=A0;
+int voltimetro1=A1;
+int voltimetro2=A2;
+int voltimetro3=A3;
+int estadoVoltimetros[6];
+int estadoFuentes[3]={1,0,0};//El primer valor va a decir si está seleccionada la fuente 1 o la 2, si lo ves muy lioso lo cambiamos
+
 
 
 void setup ()
@@ -33,14 +44,15 @@ void muxHandler ()
 
 	for (i=0; i<2; i++)
 	{
-		for (i=0; i<2; i++)
+		for (ii=0; ii<2; ii++)
 		{
-			for (i=0; i<2; i++)
+			for (iii=0; iii<2; iii++)//creo que aqui tenias un error porque todos los contadores se llamaban i, cambiado
 			{
 				digitalWrite(C, i);
 				digitalWrite(B, ii);
 				digitalWrite(A, iii);
 				muxRead[c] = digitalRead(Y);
+				estadoActualBotones[c] = muxRead[c];//esto es lo que no sé si he entendido bien que tu dices que hacen falta dos vectores...
 				c++;
 			}
 		}
@@ -58,6 +70,63 @@ void shiftHandler ()
 	}
 	digitalWrite(ParallelCK, HIGH);
 	digitalWrite(ParallelCK, LOW);
+}
+
+void flancoSubida()
+{
+	int i;
+
+	for(i=0; i<8; i++)
+	{
+		if(estadoActualBotones[i]!=estadoAnteriorBotones[i]&&estadoAnteriorBotones==0)
+		{
+			actualizarEstado(estadoActualBotones);
+		}
+	}
+	
+}
+
+void actualizarEstado()
+{
+
+	if(estadoActualBotones[1] == 1)
+		estadoFuentes[estadoFuentes[0]]+=20;//Ajuste grueso
+	if(estadoActualBotones[2] == 1)
+		estadoFuentes[estadoFuentes[0]]+=5;//Ajuste fino
+	if(estadoActualBotones[3] == 1)
+		estadoFuentes[estadoFuentes[0]]-=20;//Ajuste grueso
+	if(estadoActualBotones[4] == 1)
+		estadoFuentes[estadoFuentes[0]]-=5;//Ajuste fino
+	if(estadoActualBotones[5] == 1&&estadoFuentes[0]==2)//Cambiar
+		estadoFuentes[0]=1;
+	else if(estadoActualBotones[5] == 1&&estadoFuentes[0]==1)
+	    estadoFuentes[0]=2;
+	/*
+
+	No me acuerdo del resto de funciones:
+
+		if(estadoActual[6] == 1)
+		
+		if(estadoActual[7] == 1)
+		
+		if(estadoActual[8] == 1)
+		
+	*/
+
+}
+void lecturaVoltimetros(){
+
+	estadoVoltimetros[0] = analogRead(voltimetro0);
+	estadoVoltimetros[1] = analogRead(voltimetro1);
+	estadoVoltimetros[2] = analogRead(voltimetro2);
+	estadoVoltimetros[3] = analogRead(voltimetro3);
+	/*
+	Retroalimentación
+	
+	estadoVoltimetros[4] = analogRead(pin);
+	estadoVoltimetros[5] = analogRead(pin);
+	
+	*/
 }
 
 void loop ()
